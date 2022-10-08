0d020ea37714dbac5c906066ae3dd8e9c236d636
choiyongseok90 <choi.yongseok@yanolja.com>
2022-10-09 04:13:37 +0900

    test: 자동차 전진/중지 테스트와, 공동우승 winner 선정 테스트

--- src/test/java/racingcar/model/car/CarTest.java
+++ src/test/java/racingcar/model/car/CarTest.java
@@ -1,5 +1,25 @@
 package racingcar.model.car;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import racingcar.model.strategy.RandomNumberMoveStrategy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class CarTest {
+
+    @Test
+    void 이동전략에_따라_자동차_전진_정지_테스트(){
+        RandomNumberMoveStrategy strategy = new RandomNumberMoveStrategy();
+        Car moveCar = new Car("move");
+        Car stopCar = new Car("stop");
+
+        strategy.setMoveLimit(0);
+        moveCar.move(strategy);
+        assertEquals(moveCar.getPosition(),1);
+
+        strategy.setMoveLimit(10);
+        stopCar.move(strategy);
+        assertEquals(stopCar.getPosition(),0);
+    }
 
 }
